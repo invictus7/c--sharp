a631c6ac041aa3f7b35869fca884f5a201e64cbb
invictus7 <volodya.invirtus.85@gmail.com>
2017-02-20 15:42:06 +0200

    pal

---
+++ palindrom/palindrom/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] palindrom = new string[5];
+            palindrom[0] = Console.ReadLine();
+            palindrom[1] = Console.ReadLine();
+            palindrom[2] = Console.ReadLine() ;
+            palindrom[3] = Console.ReadLine() ;
+            palindrom[4] = Console.ReadLine();
+            for (int i = 0; i < 5; i++) ;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("__________________");
+                Console.WriteLine(palindrom[i]);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                if (palindrom[4] == palindrom[0] && palindrom[3] == palindrom[1])
+            {
+            }
+
+            }
+            Console.WriteLine("palindrom");
+
+        }
+
+    }
+
+}
