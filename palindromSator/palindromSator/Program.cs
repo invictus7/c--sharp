eeb299e8248fd17dd62b8c1f8c8ed85af369b285
invictus7 <volodya.invirtus.85@gmail.com>
2017-02-20 15:52:53 +0200

    sator

--- palindromSator/palindromSator/Program.cs
+++ palindromSator/palindromSator/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,30 +10,30 @@ namespace palindrom
     {
         static void Main(string[] args)
         {
-            string [] palindrom = new string[5];
+            Console.WriteLine("enter 1 symbol pro string");
+          string[] palindrom = new string[5];
             palindrom[0] = Console.ReadLine();
             palindrom[1] = Console.ReadLine();
-            palindrom[2] = Console.ReadLine() ;
-            palindrom[3] = Console.ReadLine() ;
+            palindrom[2] = Console.ReadLine();
+            palindrom[3] = Console.ReadLine();
             palindrom[4] = Console.ReadLine();
             for (int i = 0; i < 5; i++) ;
+            Console.WriteLine("__________________");
 
             for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine("__________________");
                 Console.WriteLine(palindrom[i]);
             }
-            for (int i = 0; i < 5; i++)
-            {
+            
                 if (palindrom[4] == palindrom[0] && palindrom[3] == palindrom[1])
-            {
-            }
+                {
+                Console.WriteLine("palindrom");
 
             }
-            Console.WriteLine("palindrom");
+
+        }
 
         }
 
     }
 
-}
