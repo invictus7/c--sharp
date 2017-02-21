dacd0fb5962d111ce1683ae5c5f2e6ec90b33586
invictus7 <volodya.invirtus.85@gmail.com>
2017-02-21 15:04:55 +0200

    added diagonal

---
+++ hw.4palindromSator/palindromSator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter 1 symbol pro string");
+            string palindrom = Console.ReadLine();
+            //palindrom[0] = Console.ReadLine();
+            //palindrom[1] = Console.ReadLine();
+            //palindrom[2] = Console.ReadLine();
+            //palindrom[3] = Console.ReadLine();
+            //palindrom[4] = Console.ReadLine();
+            //for (int i = 0; i < 5; i++) ;
+            Console.WriteLine("__________________");
+
+            {
+            for (int j = 0; j < palindrom.Length ; j++)
+                Console.WriteLine(palindrom[j]);
+            }
+
+
+            int n = palindrom.Length;
+            int i = 0;
+
+            while (palindrom[i] == palindrom[n - 1 - i] && i < n / 2)
+            {
+                Console.WriteLine(i+ " - " + (n - 1 - i));
+                i = i + 1;
+            }
+            Console.WriteLine(i);
+
+            //if (i == n / 2)
+            //{
+            //    Console.WriteLine("palindrom");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("eror");
+
+            //}
+            //Console.Read();
+
+        }
+        //(palindrom[4] == palindrom[0] && palindrom[3] == palindrom[1])
+        //   {
+        //   Console.WriteLine("palindrom");if
+
+
+
+    }
+
+        }
+
+    
+
---
+++ hw4.matrix.Diagonal/ConsoleApplication1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int[,] matrix = new int[n,n ];
+
+
+           // for (int i = 0; i < 4; i++) ;
+            //for (int j = 0; j < 4; j++) ;
+
+            for (int i = 0; i < n-1; i++)
+            {
+                for (int j = 0; j < n-1; j++)
+                {
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+
+
+            }
+
+            Console.WriteLine("_______________");
+
+
+            for (int i = 0; i < n-1; i++)
+            {
+                for (int j = 0; j < n-1; j++)
+                {
+                    Console.Write("{0}\t", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+           
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - 1; j++)
+                {
+                   // int sum = 0;
+                   // Console.WriteLine( sum=i+j);
+                }
+            }
+
+
+
+                    //int MainDiagonal = (matrix[0, 0] + matrix[1, 1] + matrix[2, 2] + matrix[3, 3]);
+                    //Console.WriteLine("________");
+                    //Console.WriteLine(MainDiagonal);
+                    //Console.WriteLine();
+
+
+
+                }
+
+    }
+}
